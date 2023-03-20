bd972e6edfe37c5bff9da796a424859719ecf504
David Sjöblom <davidsjoblom1@hotmail.com>
2023-03-20 16:46:39 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 import 'normalize.css';
@@ -10,8 +10,9 @@ import './styles/numberHeadings.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/40k-bolt-action-react-app">
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
+// basename="/40k-bolt-action-react-app"
