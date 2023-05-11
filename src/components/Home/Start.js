de24cac57c7d3844750af1cf5bca76456f9e8b9e
David Sjöblom <davidsjoblom1@hotmail.com>
2023-05-11 22:04:59 +0200

    wip

--- src/components/Home/Start.js
+++ src/components/Home/Start.js
@@ -5,9 +5,9 @@ export default function Start() {
     <>
       <h1 className="start-page-title">Blowback</h1>
       <hr />
-      <h3 className="no-count start-page-subtitle">
+      <h2 className="no-count start-page-subtitle">
         Miniatures Wargame System
-      </h3>
+      </h2>
       <img src={pageArt} alt="" className="page-art" />
     </>
   );
